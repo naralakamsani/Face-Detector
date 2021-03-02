1e2bc9dff5c436883740a34f5e6d979d7793215c
naralakamsani <nc.lakamsani@gmail.com>
2021-03-02 18:54:28 -0500

    added text

--- face_detector(video).py
+++ face_detector(video).py
@@ -1,35 +1,44 @@
-#imports
 import cv2, time
 
-#Use CascadeClassifier  to define how a face looks like to the computer
-face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+consistent_face = 0
+x_old = 0
+y_old = 0
 
-#Start capturing Video
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 video = cv2.VideoCapture(0)
 
 while True:
     check, frame = video.read()
-    
-    #Apply grey filter to each frame
-    grey_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    
-    #Detect if the current frame contains a face
-    faces = face_cascade.detectMultiScale(grey_frame, scaleFactor=1.1, minNeighbors=5)
-    
-    #Box the face found in the frame
-    for x, y, w, h in faces:
-        frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
 
-    #Show the frame after box on the face has been applied
+    grey_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(grey_frame, scaleFactor=1.1, minNeighbors=5)
+
+    for x, y, w, h in faces:
+        predicted_group = "50% confident"
+        if (x_old + 15 > x > x_old - 15) and (y_old + 15 > y > y_old - 15):
+            consistent_face = consistent_face + 1
+        else:
+            consistent_face = 0
+
+        if consistent_face > 5:
+            predicted_group = "100% confident"
+
+        x_old = x
+        y_old = y
+
+        frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+        cv2.putText(frame, predicted_group, (x, y - 5), cv2.FONT_ITALIC, 1, (0, 255, 0), 2)
+
+        face_frame = frame[y - 50:y + h + 50, x - 50:x + w + 50]
+
     cv2.imshow("video", frame)
 
-    #Reads the input from keyboard
+    # If key is q: exit
     key = cv2.waitKey(1)
-
-    #quit when q is pressed
     if key == ord('q'):
         break
-        
-#Close al windows and stop recording
+
 video.release()
 cv2.destroyAllWindows()
+
